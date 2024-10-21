c39a6a8bb8ba3cb4f163e0b2d35af7a04efeb55c
Garrido-cod <juamgarrido96@gmail.com>
2024-10-21 14:56:34 -0300

    Versão teste 1

---
+++ js/carrinho copy.js
@@ -0,0 +1,15 @@
+var temp;
+var a1=12; 
+var a2=77;
+var a3=23;
+
+temp=a1;
+if(a2>temp)
+temp=a2;
+
+if(a3>temp)
+temp=a3;
+
+console.log(temp);
+
+
---
+++ js/carrinho.js
@@ -0,0 +1,33 @@
+
+function alterarQtd(produto, acao){
+    const valor = document.getElementById("valor_" + produto)
+    const qtd = document.getElementById("qtd_" + produto)
+    const total = document.getElementById("total_" + produto)
+    
+    if(acao == '-' && qtd.innerHTML <= 0){
+        alert("quantidade menor que o permitido")
+    }else{
+    acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--
+    const valorTotal= qtd.innerHTML * somenteNumeros(valor.innerHTML)
+    total.innerHTML = formatarValor(valorTotal)
+    soma()
+    }
+}
+
+function soma (){
+    let soma = 0
+    for(let i = 1; i < 4; i++){
+        let numeros = somenteNumeros(document.getElementById("total_"+ i).innerHTML)
+        soma += Number(numeros)
+    }
+    document.getElementById("totalsub").innerHTML = formatarValor(soma)
+}
+
+function somenteNumeros(n){
+    return n.replace(/\D/g, '')
+}
+
+function formatarValor(n){
+    return 'R$' + n.toLocaleString('pt-BR')
+}
+
